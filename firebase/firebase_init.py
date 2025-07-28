fb5c20c687a04ead1c25779d8d73b1dbb150b182
deepalquemy2119 <deepalquemy2119@gmail.com>
2025-07-28 12:52:12 -0300

    firebase_init.py

--- firebase/firebase_init.py
+++ firebase/firebase_init.py
@@ -1,5 +1,3 @@
-
-
 import os
 import json
 import firebase_admin
@@ -8,25 +6,27 @@ from firebase_admin import credentials, db
 from dotenv import load_dotenv
 load_dotenv()
 
-# Obtener las variables desde el entorno
 firebase_db_url = os.getenv("FIREBASE_DB_URL")
-firebase_cred_path = os.getenv("FIREBASE_CRED_PATH")
+
+# Elegir ruta de credenciales según entorno
+if os.path.exists("/run/secrets/firebase-key.json"):
+    firebase_cred_path = "/run/secrets/firebase-key.json"
+else:
+    firebase_cred_path = os.getenv("FIREBASE_CRED_PATH") or "./firebase/firebase-key.json"
+
+print(f"DEBUG: FIREBASE_DB_URL = {firebase_db_url}")
+print(f"DEBUG: FIREBASE_CRED_PATH = {firebase_cred_path}")
 
 if not firebase_db_url or not firebase_cred_path:
     raise Exception("FIREBASE_DB_URL y/o FIREBASE_CRED_PATH no están configuradas")
 
-
-
-
 with open(firebase_cred_path) as f:
     cred_dict = json.load(f)
 
-# Inicializar app de Firebase
 cred = credentials.Certificate(cred_dict)
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': firebase_db_url
 })
 
-# Obtener referencia al RTDB
 def get_firebase_db():
     return db
