59bbbcb3cd4c8bcd422f284f04ed4f9058480350
deepalquemy2119 <deepalquemy2119@gmail.com>
2025-08-01 19:58:43 -0300

    import ddbb to railway

--- config.py
+++ config.py
@@ -1,39 +1,27 @@
-
-
+from urllib.parse import quote_plus
 import os
 from dotenv import load_dotenv
 import mysql.connector
 
-
-# Cargamos variables del .env
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'clave_por_defecto')
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-   
 
-    # Base de datos
     MYSQL_PORT = os.getenv("DB_PORT", 3306)
     MYSQL_HOST = os.getenv("DB_HOST")
     MYSQL_USER = os.getenv("DB_USER")
     MYSQL_PASSWORD = os.getenv("DB_PASSWORD")
     MYSQL_DATABASE = os.getenv("DB_NAME")
 
+    # Aplicar url encoding a la contraseña para evitar errores con caracteres especiales
     SQLALCHEMY_DATABASE_URI = (
-    f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-)
+        f"mysql+mysqlconnector://{MYSQL_USER}:{quote_plus(MYSQL_PASSWORD)}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-    # --------- Para subir materiales para crear Cursos ----- viene de app/_init_.py ------
-    # para local ---> SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario:password@localhost/ddbb_antares_project'
-    
-
-
-
 
     # Firebase
     FIREBASE_DB_URL = os.getenv("FIREBASE_DB_URL")
@@ -43,7 +31,7 @@ class Config:
     CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
     CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
-  # Mail (Gmail con contraseña de aplicación)
+    # Mail (Gmail con contraseña de aplicación)
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USERNAME = os.getenv('GMAIL_USER')
@@ -53,16 +41,11 @@ class Config:
 
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'no-reply@antares.com')
 
-
-    # Carpeta de subida de archivos
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-    
+
 
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/test_db'
     FIREBASE_DB_URL = 'https://<tu-test-url>.firebaseio.com/'
     FIREBASE_CRED_PATH = '/firebase/clave_privada.json'
-
-
