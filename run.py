b163d3eca013e5419ed515d619f701577e0f4691
deepalquemy2119 <deepalquemy2119@gmail.com>
2025-08-01 17:05:29 -0300

    railway-deploy

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
-
-import sys
 import os
-import mysql.connector
-
 from app import create_app
 
 app = create_app()
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)),debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
